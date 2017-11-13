659043353115f90bc39722101ebcf0eeaa98b4d0
blackflux <blackflux@unfine.de>
2017-11-12 20:03:31 -0800

    - coverage folder now gets deleted before run to ensure generation - committed some remaining files

--- tests/test_gardener.js
+++ tests/test_gardener.js
@@ -2,11 +2,13 @@ const path = require('path');
 const expect = require("chai").expect;
 const grunt = require('grunt');
 const gardener = require('../lib/gardener');
+const rimraf = require('rimraf');
 
 describe("Testing Gardener", () => {
   // eslint-disable-next-line func-names
   it("Testing Execution", function (done) {
     this.timeout(60000);
+    rimraf.sync(path.join(__dirname, 'mock', 'coverage'));
     grunt.config.init({
       silently: true,
       gardener: {
