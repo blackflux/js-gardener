c894ec7d8fdc0923f58add77b5577627a1322f53
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-04-04 16:41:42 -0700

    deleted old files

--- test/mock/test/test_hello.js
+++
@@ -1,6 +0,0 @@
-const expect = require("chai").expect;
-const hello = require('../lib/hello');
-
-describe("Testing Module", () => it("Testing Addition", () => {
-  expect(hello()).to.equal('Hello World');
-}));
