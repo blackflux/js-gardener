621fc1c9454569dc1e97d812675e1554a4d7a114
Lukas Siemon <lukas.siemon@gmail.com>
2019-04-07 15:48:41 -0700

    fix: updated deps

--- src/subtasks/robo.js
+++ src/subtasks/robo.js
@@ -1,8 +1,12 @@
-const path = require('path');
 const robo = require('robo-config');
 
 module.exports = (logger, cwd) => new Promise((resolve, reject) => {
-  const result = robo(path.join(cwd, '.roboconfig.json'));
-  result.forEach(l => logger.error(l));
-  return result.length === 0 ? resolve() : reject();
+  try {
+    const result = robo(cwd);
+    result.forEach(l => logger.error(l));
+    return result.length === 0 ? resolve() : reject(result);
+  } catch (e) {
+    logger.error(e);
+    return reject(e);
+  }
 });
