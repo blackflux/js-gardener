27f0f9094c61e7b7c19e0ae73f491b164946b275
Kodi Ackerman <kodiackerman@msn.com>
2019-11-28 10:29:40 -0800

    fix: depcheck error for node:13

--- test/subtasks/depcheck.spec.js
+++ test/subtasks/depcheck.spec.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const childProcess = require('child_process');
 const expect = require('chai').expect;
 const sfs = require('smart-fs');
 const { describe } = require('node-tdd');
 const depcheck = require('../../src/subtasks/depcheck');
 const exec = require('../../src/util/exec');
+
 
 describe('Testing depcheck', { timeout: 30000, record: console, useTmpDir: true }, () => {
   it('Testing Not Installed (NPM)', async ({ dir, recorder, capture }) => {
@@ -17,10 +19,14 @@ describe('Testing depcheck', { timeout: 30000, record: console, useTmpDir: true 
   });
 
   it('Testing Ok (NPM)', async ({ dir, recorder }) => {
+    // todo: remove monkey patch once circleci container is updated
+    const spawnSyncOriginal = childProcess.spawnSync;
+    childProcess.spawnSync = () => ({ stdout: '', stderr: '' });
     sfs.smartWrite(path.join(dir, 'package.json'), { dependencies: {} });
     sfs.smartWrite(path.join(dir, 'package-lock.json'), { lockfileVersion: 1 });
     await depcheck(console, dir);
     expect(recorder.get()).to.deep.equal([]);
+    childProcess.spawnSync = spawnSyncOriginal;
   });
 
   it('Testing Not Installed (YARN)', async ({ dir, recorder, capture }) => {
