27f2358c4511efb3061a3e8c9cb63b0342ff0b2a
Lukas Siemon <github@blackflux.com>
2021-02-24 12:38:10 -0800

    fix: removed audit form js-gardener (should be in ci)

--- src/util/exec.js
+++ src/util/exec.js
@@ -1,5 +1,3 @@
-const { execSync, exec } = require('child_process');
+const { execSync } = require('child_process');
 
-module.exports.run = (cmd, cwd, cb = undefined) => (cb
-  ? exec(cmd, { cwd }, cb)
-  : execSync(cmd, { cwd }).toString('utf8').trim());
+module.exports.run = (cmd, cwd) => execSync(cmd, { cwd }).toString('utf8').trim();
